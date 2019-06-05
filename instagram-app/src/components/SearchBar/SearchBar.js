619819d3ad9d470cbd525789fa881da91169a3cc
Dave Irwin <david@redirwin.com>
2019-06-05 12:59:08 -0700

    WIP - Converted CommentSection to class component.

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -9,42 +9,44 @@ import {
 } from "@fortawesome/free-regular-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-const SearchBar = () => {
-  return (
-    <div className="header-container">
-      <div className="header-left-container">
-        <FontAwesomeIcon
-          icon={faInstagram}
-          size="lg"
-          className="insta-icon header-icons"
-        />
-        <div className="bar"> </div>
-        <div className="app-name">Instaclone</div>
-      </div>
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="header-container">
+        <div className="header-left-container">
+          <FontAwesomeIcon
+            icon={faInstagram}
+            size="lg"
+            className="insta-icon header-icons"
+          />
+          <div className="bar"> </div>
+          <div className="app-name">Instaclone</div>
+        </div>
 
-      <form className="search-form-container">
-        <input type="text" name="search-input" placeholder="&#9906; Search" />
-      </form>
+        <form className="search-form-container">
+          <input type="text" name="search-input" placeholder="&#9906; Search" />
+        </form>
 
-      <div className="header-icons-container">
-        <FontAwesomeIcon
-          icon={faCompass}
-          className="explore-icon header-icons"
-          size="lg"
-        />
-        <FontAwesomeIcon
-          icon={faComment}
-          size="lg"
-          className="comment-icon header-icons"
-        />
-        <FontAwesomeIcon
-          icon={faHeart}
-          size="lg"
-          className="heart-icon header-icons"
-        />
+        <div className="header-icons-container">
+          <FontAwesomeIcon
+            icon={faCompass}
+            className="explore-icon header-icons"
+            size="lg"
+          />
+          <FontAwesomeIcon
+            icon={faComment}
+            size="lg"
+            className="comment-icon header-icons"
+          />
+          <FontAwesomeIcon
+            icon={faHeart}
+            size="lg"
+            className="heart-icon header-icons"
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SearchBar;
