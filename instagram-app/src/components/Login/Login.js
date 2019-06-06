f708373537e97153bfacda66f322ae9cd41efbd7
Dave Irwin <david@redirwin.com>
2019-06-05 23:41:01 -0700

    Adding local storage of username to login function.

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -3,22 +3,39 @@ import React, { Component } from "react";
 class Login extends Component {
   state = {
     username: "",
-    password: ""
+    password: "",
+    loggedIn: false
+  };
+
+  manageLoginInput = e => {
+    console.log(e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   };
 
   login = e => {
     e.preventDefault();
+    const usernameInput = this.state.username;
+    // console.log(usernameInput);
+    localStorage.setItem("username", usernameInput);
+
+    this.setState({
+      username: "",
+      password: ""
+    });
   };
 
   render() {
     return (
-      <form>
+      <form onSubmit={e => this.login(e)}>
         <input
           type="text"
           className="username-input"
           name="username"
           placeholder="User Name"
           value={this.state.username}
+          onChange={e => this.manageLoginInput(e)}
         />
         <input
           type="text"
@@ -26,6 +43,7 @@ class Login extends Component {
           name="password"
           placeholder="Password"
           value={this.state.password}
+          onChange={e => this.manageLoginInput(e)}
         />
         <button className="login-submit-button" type="submit" value="Submit">
           Submit
