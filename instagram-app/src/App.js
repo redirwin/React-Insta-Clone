80f39710388cb1ac602532787abea03d7d7a7007
Dave Irwin <david@redirwin.com>
2019-06-05 21:57:12 -0700

    Created PostsPage and refactored from App.js.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,43 +1,13 @@
 import React, { Component } from "react";
 import "./App.scss";
-import dummyData from "./dummy-data";
-import PostContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
+// import dummyData from "./dummy-data";
+// import PostContainer from "./components/PostContainer/PostContainer";
+import PostsPage from "./components/PostContainer/PostsPage";
+// import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends Component {
-  state = {
-    posts: []
-  };
-
-  componentDidMount() {
-    this.setState({ posts: dummyData });
-  }
-
-  filterPosts = (e, input) => {
-    e.preventDefault();
-    if (input) {
-      this.setState({
-        posts: dummyData.filter(data => data.username === input)
-      });
-    } else {
-      this.setState({
-        posts: dummyData
-      });
-    }
-  };
-
   render() {
-    return (
-      <div className="app-container">
-        <SearchBar
-          filterPosts={this.filterPosts}
-          searchInput={this.state.searchInput}
-        />
-        <div className="App">
-          <PostContainer posts={this.state.posts} />
-        </div>
-      </div>
-    );
+    return <PostsPage />;
   }
 }
 
---
+++ instagram-app/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import dummyData from "../../dummy-data";
+import PostContainer from "./PostContainer";
+import SearchBar from "../SearchBar/SearchBar";
+
+class PostsPage extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+
+  filterPosts = (e, input) => {
+    e.preventDefault();
+    if (input) {
+      this.setState({
+        posts: dummyData.filter(data => data.username === input)
+      });
+    } else {
+      this.setState({
+        posts: dummyData
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <div className="app-container">
+          <SearchBar
+            filterPosts={this.filterPosts}
+            searchInput={this.state.searchInput}
+          />
+          <div className="App">
+            <PostContainer posts={this.state.posts} />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default PostsPage;
