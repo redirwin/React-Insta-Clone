ee17973173c263f6eeaef84f20239c956d542a08
Dave Irwin <david@redirwin.com>
2019-06-05 21:57:48 -0700

    Removed unused imports from App.js.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import "./App.scss";
-// import dummyData from "./dummy-data";
-// import PostContainer from "./components/PostContainer/PostContainer";
 import PostsPage from "./components/PostContainer/PostsPage";
-// import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends Component {
   render() {
