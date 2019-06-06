3caa030263cdfc8dd70cf17ec2d9d9e1cc30ef9d
Dave Irwin <david@redirwin.com>
2019-06-05 19:04:16 -0700

    WIP - Converted SearchBar back to functional component. Removed AddComments and placed functionality into CommentSection.

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -9,44 +9,42 @@ import {
 } from "@fortawesome/free-regular-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-class SearchBar extends React.Component {
-  render() {
-    return (
-      <div className="header-container">
-        <div className="header-left-container">
-          <FontAwesomeIcon
-            icon={faInstagram}
-            size="lg"
-            className="insta-icon header-icons"
-          />
-          <div className="bar"> </div>
-          <div className="app-name">Instaclone</div>
-        </div>
-
-        <form className="search-form-container">
-          <input type="text" name="search-input" placeholder="&#9906; Search" />
-        </form>
-
-        <div className="header-icons-container">
-          <FontAwesomeIcon
-            icon={faCompass}
-            className="explore-icon header-icons"
-            size="lg"
-          />
-          <FontAwesomeIcon
-            icon={faComment}
-            size="lg"
-            className="comment-icon header-icons"
-          />
-          <FontAwesomeIcon
-            icon={faHeart}
-            size="lg"
-            className="heart-icon header-icons"
-          />
-        </div>
+const SearchBar = props => {
+  return (
+    <div className="header-container">
+      <div className="header-left-container">
+        <FontAwesomeIcon
+          icon={faInstagram}
+          size="lg"
+          className="insta-icon header-icons"
+        />
+        <div className="bar"> </div>
+        <div className="app-name">Instaflam</div>
       </div>
-    );
-  }
-}
+
+      <form className="search-form-container">
+        <input type="text" name="search-input" placeholder="&#9906; Search" />
+      </form>
+
+      <div className="header-icons-container">
+        <FontAwesomeIcon
+          icon={faCompass}
+          className="explore-icon header-icons"
+          size="lg"
+        />
+        <FontAwesomeIcon
+          icon={faComment}
+          size="lg"
+          className="comment-icon header-icons"
+        />
+        <FontAwesomeIcon
+          icon={faHeart}
+          size="lg"
+          className="heart-icon header-icons"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
