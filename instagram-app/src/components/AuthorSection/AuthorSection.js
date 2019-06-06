da87d522ac983b28a2c2861b5571e07de59b2589
Dave Irwin <david@redirwin.com>
2019-06-06 16:58:02 -0700

    Refactored AuthorSection to styled-components.

--- instagram-app/src/components/AuthorSection/AuthorSection.js
+++ instagram-app/src/components/AuthorSection/AuthorSection.js
@@ -1,18 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../AuthorSection/AuthorSection.scss";
+// import "../AuthorSection/AuthorSection.scss";
+import styled from "styled-components";
+
+const AboutAuthor = styled.div`
+  background-color: #ffffff;
+  border-top: 1px solid grey;
+  border-left: 1px solid grey;
+  border-right: 1px solid grey;
+  padding: 2%;
+  display: flex;
+
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    padding: 3px;
+    border: 1px solid red;
+  }
+  div {
+    align-self: center;
+    margin-left: 2%;
+    font-weight: bold;
+  }
+`;
+
+const PostImageContainer = styled.div`
+  .post-image {
+    img {
+      width: 100%;
+      border-left: 1px solid grey;
+      border-right: 1px solid grey;
+    }
+  }
+`;
+
+const PostImage = styled.img`
+  width: 100%;
+  border-left: 1px solid grey;
+  border-right: 1px solid grey;
+`;
 
 const AuthorSection = props => {
   return (
-    <div className="author-section">
-      <div className="about-author">
+    <AuthorPost>
+      <AboutAuthor>
         <img src={props.thumbnail} alt="Post Author Thumnail" />
         <div>{props.author}</div>
-      </div>
-      <div className="post-image">
-        <img src={props.postContent} alt="Post Content" />
-      </div>
-    </div>
+      </AboutAuthor>
+      <PostImageContainer>
+        <PostImage src={props.postContent} alt="Post Content" />
+      </PostImageContainer>
+    </AuthorPost>
   );
 };
 
